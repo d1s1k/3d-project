c3de185f0f611c0dd208e6ddc5381105d881078a
d1s1k <47217802+d1s1k@users.noreply.github.com>
2019-01-31 18:22:26 +0300

    Add files via upload

---
+++ game/Assets/mouselook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour { 
+
+    public enum rotateonAxes
+{
+    MouseXAndY = -0,
+        mouseX = 1,
+        mouseY = 2,
+
+
+
+
+}
+
+    public rotateonAxes axes = rotateonAxes.MouseXAndY;
+     public float sensivitiHor = 9.0f
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(axes == rotateonAxes.mouseX);
+        {
+
+            transform.rotation(0, Input.GetAxis("mouse X") * sensivitiHor, 0);
+        }else if (axes == rotateonAxes.mouseY) ;
+
+        else;
+
+
+
+    }
+}
